5b952bc416f4b6fbe5e41e4e824b6441fb031934
Simon <smnkrmp@gmail.com>
2022-01-09 22:49:24 +0100

    Added pi4j and Listener class to read Button Matrix

---
+++ src/main/java/Simon/ButtonMatrix/Listener.java
@@ -0,0 +1,72 @@
+package Simon.ButtonMatrix;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class Listener {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		final GpioController gpio = GpioFactory.getInstance();
+		
+		char[][] matrix = {	{'1', '2', '3', '+'},
+							{'3', '5', '6', '-'},
+							{'7', '8', '9', '*'},
+							{'.', '0', '#', '/'}};
+		
+		//Rows
+		final GpioPinDigitalInput row1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_21, PinPullResistance.PULL_UP);
+		final GpioPinDigitalInput row2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_22, PinPullResistance.PULL_UP);
+		final GpioPinDigitalInput row3 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_23, PinPullResistance.PULL_UP);
+		final GpioPinDigitalInput row4 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_24, PinPullResistance.PULL_UP);
+		GpioPinDigitalInput[] row = {row1, row2, row3, row4};
+		
+		//Columns
+		final GpioPinDigitalOutput column1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, PinState.HIGH);
+		final GpioPinDigitalOutput column2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_10, PinState.HIGH);
+		final GpioPinDigitalOutput column3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, PinState.HIGH);
+		final GpioPinDigitalOutput column4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, PinState.HIGH);
+		GpioPinDigitalOutput[] column = {column1, column2, column3, column4};
+		
+		//Shutdown options
+		row1.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+		row2.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+		row3.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+		row4.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+		
+		column1.setShutdownOptions(true, PinState.LOW);
+		column2.setShutdownOptions(true, PinState.LOW);
+		column3.setShutdownOptions(true, PinState.LOW);
+		column4.setShutdownOptions(true, PinState.LOW);
+		
+		while (true) {
+			
+			for (int i = 0; i < 4; i++) {
+				
+				column[i].low();
+				
+				for (int j = 0; i < 4; i++) {
+					
+					if(row[j].isLow()) {
+						
+						System.out.println(matrix[i][j]);
+						
+					}
+					
+				}
+				
+				column[i].high();
+				
+				
+			}
+			
+		}
+		
+	}
+	
+}
